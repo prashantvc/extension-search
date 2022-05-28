82ca1d72a53ad505f7203804469be0a2905f0bf4
Prashant Cholachagudda <pvc@outlook.com>
2022-05-29 03:07:18 +0530

    Replaced the results list

--- src/App.tsx
+++ src/App.tsx
@@ -2,29 +2,48 @@ import { initializeIcons, Stack } from "@fluentui/react";
 import { useState } from "react";
 import "./App.css";
 import { Extension } from "./components/ResultCard";
-import ResultList from "./components/ResultList";
+import { MyExtension, ResultList } from "./components/MyList";
 import Searchbox from "./components/Searchbox";
 
 function App() {
-    const [results, setResults] = useState<Extension[]>([]);
+    const [results, setResults] = useState<MyExtension[]>([]);
 
     initializeIcons();
 
     function addResults(extensions: Extension[]) {
-        setResults(extensions);
+        var localResults = extensions.map((result: Extension) => ({
+            icon: getImageUrl(result),
+            name: result.displayName,
+            version: result.versions[0].version,
+            publisher: result.publisher.displayName,
+            description: result.shortDescription,
+        }));
+
+        setResults(localResults);
     }
 
     return (
-        <div className="centered">
+        <div>
             <Stack tokens={{ childrenGap: 20 }}>
-                <Stack>
+                <Stack className="centered">
                     <h2>Search Extensions</h2>
                     <Searchbox addResults={addResults} />
                 </Stack>
-                <ResultList results={results} />
+                <div style={{ overflowY: "scroll", height: "70vh" }}>
+                    <ResultList results={results} />
+                </div>
             </Stack>
         </div>
     );
+}
+
+function getImageUrl(ext: Extension) {
+    var imageSource =
+        ext.versions[0].files.length > 0
+            ? ext.versions[0].files[1].source
+            : "https://cdn.vsassets.io/v/M203_20220518.4/_content/Header/default_icon_128.png";
+
+    return imageSource;
 }
 
 export default App;
